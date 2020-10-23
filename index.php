1c44041e560d1c7ff2dd2900914d312df8587ee7
lvzhiwei <641011970@qq.com>
2020-10-23 10:33:27 +0800

    用户1

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ $b = 2;
 $c = 3;
 $sum = $a + $b*2;
 
-echo $sum;
+//echo $sum;
 
 interface Test
 {
@@ -21,10 +21,27 @@ class User implements Test
     public function echoHello()
     {
         // TODO: Implement echoHello() method.
+        $name = 'lzw';
+        $age = 20;
+
+        echo $this->hello();
     }
 
     public function hello()
     {
         // TODO: Implement hello() method.
+        $this->test(1,2,3);
+        return "Hello, my name is lzw, age is 18";
+    }
+
+    public function test($a, $b, $c)
+    {
+        $sum = $a + $b;
+        $sum += $c;
+        $city = 'sz';
+
+        echo $sum . PHP_EOL;
     }
 }
+
+(new User())->echoHello();
